17cbe41b97695ca14a4012c88dd70f3b6e057214
chetanraja <chetankankanala699@gmail.com>
2020-08-06 23:25:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,6 @@ def process():
 	            
  return render_template('index.html',user_input=user_input,bot_response=bot_response)
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 		
